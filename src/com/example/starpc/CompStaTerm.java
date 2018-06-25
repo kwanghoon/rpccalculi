dd288336eb7203675d65a2d72c5e6e9262c07669
Gayoung Kim <qrkdudq4301@naver.com>
2018-06-25 09:32:21 +0900

    CompStaTerm 완료

--- src/com/example/starpc/CompStaTerm.java
+++ src/com/example/starpc/CompStaTerm.java
@@ -55,6 +55,52 @@ public class CompStaTerm {
 		}
 		else if (tt instanceof com.example.typedrpc.App) {
 			com.example.typedrpc.App ttApp = (com.example.typedrpc.App) tt;
+			
+			if (ttApp.getLoc() instanceof com.example.typedrpc.LocType) {
+				com.example.typedrpc.LocType loc = (com.example.typedrpc.LocType) ttApp.getLoc();
+				
+				if (loc.getLoc() == Location.Client) {
+					String fvar = "f" + i;
+					String xvar = "x" + (i + 1);
+					String rvar = "r" + (i + 2);
+					Var f = new Var(fvar);
+					Var x = new Var(xvar);
+					Var r = new Var(rvar);
+					
+					Pair<Integer, StaTerm> p1 = compClient(i + 3, ttApp.getFun());
+					Pair<Integer, StaTerm> p2 = compClient(p1.getKey(), ttApp.getArg());
+					
+					ArrayList<StaValue> xs = new ArrayList<>();
+					xs.add(x);
+					
+					Let retLet = new Let(fvar, p1.getValue(),
+										new Let(xvar, p2.getValue(),
+												new Let(rvar, new App(f, xs), r)));
+					
+					retPair = new Pair<>(p2.getKey(), retLet);
+				}
+				else {
+					String fvar = "f" + i;
+					String xvar = "x" + (i + 1);
+					String rvar = "r" + (i + 2);
+					Var f = new Var(fvar);
+					Var x = new Var(xvar);
+					Var r = new Var(rvar);
+					
+					Pair<Integer, StaTerm> p1 = compClient(i + 3, ttApp.getFun());
+					Pair<Integer, StaTerm> p2 = compClient(p1.getKey(), ttApp.getArg());
+					
+					ArrayList<StaValue> xs = new ArrayList<>();
+					xs.add(x);
+					
+					Let retLet = new Let(fvar, p1.getValue(),
+										new Let(rvar, p2.getValue(),
+												new Let(rvar, new Req(f, xs), r)));
+					retPair = new Pair<>(p2.getKey(), retLet);
+				}
+				
+				return retPair;
+			}
 		}
 
 		return null;
@@ -98,6 +144,62 @@ public class CompStaTerm {
 		}
 		else if (tt instanceof com.example.typedrpc.App) {
 			com.example.typedrpc.App ttApp = (com.example.typedrpc.App) tt;
+			
+			if (ttApp.getLoc() instanceof com.example.typedrpc.LocType) {
+				com.example.typedrpc.LocType loc = (com.example.typedrpc.LocType) ttApp.getLoc();
+				
+				if (loc.getLoc() == Location.Client) {
+					String fvar = "f" + i;
+					String xvar = "x" + (i + 1);
+					String yvar = "y" + (i + 2);
+					String zvar = "z" + (i + 3);
+					String rvar = "r" + (i + 4);
+					Var f = new Var(fvar);
+					Var x = new Var(xvar);
+					Var y = new Var(yvar);
+					Var z = new Var(zvar);
+					Var r = new Var(rvar);
+					
+					ArrayList<String> lamXs = new ArrayList<>();
+					ArrayList<StaValue> appXs = new ArrayList<>();
+					lamXs.add(zvar);
+					appXs.add(z);
+					
+					Lam commuteFun = new Lam(Location.Client, lamXs,
+											new Let(yvar, new App(f, appXs), new Ret(y)));
+					
+					Pair<Integer, StaTerm> p1 = compServer(i + 5, ttApp.getFun());
+					Pair<Integer, StaTerm> p2 = compServer(p1.getKey(), ttApp.getArg());
+					
+					ArrayList<StaValue> callXs = new ArrayList<>();
+					
+					Let retLet = new Let(fvar, p1.getValue(), 
+										new Let(xvar, p2.getValue(),
+												new Let(rvar, new Call(commuteFun, callXs), r)));
+					retPair = new Pair<>(p2.getKey(), retLet);
+					
+					}
+				else {
+					String fvar = "f" + i;
+					String xvar = "x" + (i + 1);
+					String rvar = "r" + (i + 2);
+					Var f = new Var(fvar);
+					Var x = new Var(xvar);
+					Var r = new Var(rvar);
+					
+					Pair<Integer, StaTerm> p1 = compServer(i + 3, ttApp.getFun());
+					Pair<Integer, StaTerm> p2 = compServer(p1.getKey(), ttApp.getArg());
+					
+					ArrayList<StaValue> appXs = new ArrayList<>();
+					appXs.add(x);
+					
+					Let retLet = new Let(fvar, p1.getValue(),
+										new Let(xvar, p2.getValue(),
+												new Let(rvar, new App(f, appXs), r)));
+					
+					retPair = new Pair<>(p2.getKey(), retLet);
+				}
+			}
 		}
 
 		return null;
